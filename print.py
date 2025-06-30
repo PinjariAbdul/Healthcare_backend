b04a79fd81ec5ae51fa8abf39f36be54c63dd067
abdulreigns.12345@gmail.com <abdulreigns.12345@gmail.com>
2025-06-30 13:11:07 +0530

    changes

--- print.py
+++ print.py
@@ -1,3 +1,3 @@
 from decouple import config
 print("DB_USER:", config('DB_USER'))// to know ur user name 
-print("DB_PASSWORD:", config('DB_PASSWORD'))//to know your sb password
+print("DB_PASSWORD:", config('DB_PASSWORD'))//to know your  password
